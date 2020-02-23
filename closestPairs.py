5093fd21d43cf32314e186b35d1a50871b4514d6
Andrew Breyen <abreyen@Andrews-MacBook-Pro.local>
2020-02-23 15:40:28 -0600

    add psudo code from pg 194

--- closestPairs.py
+++ closestPairs.py
@@ -18,8 +18,28 @@ numOfPoints = lines[0]
 # remove number of points from the array -- make it easier later
 lines.pop(0)
 
+nDivTwo = numOfPoints/2
 
 
+# TODO: split up lines into X and Y parts,
+# sort them according to pg 194
 
 def closestPairs(p,q):
-    print("yeet")
+    if numOfPoints <= 3:
+        #do brute force!
+    else:
+        # copy the first ⌈nDivTwo⌉ points of P to array Pl
+        # copy the same ⌈nDivTwo⌉ points from Q to array Ql
+        # copy the remaining ⌊nDivTwo⌋ points of P to array Pr
+        # copy the same ⌊nDivTwo⌋ points from Q to array Qr
+        # dl ← closestPairs(Pl, Ql)
+        # dr ← closestPairs(Pr, Qr)
+        # d ←min{dl, dr}
+        # m ← P [⌈nDivTwo⌉ − 1].x
+        # copy all the points of Q for which |x − m| < d into array S[0..num − 1] dminsq ← d2
+        # for i ← 0 to num − 2 do
+            # k←i+1
+            # while k ≤ num − 1 and (S[k].y − S[i].y)2 < dminsq
+                # dminsq ← min((S[k].x − S[i].x)2+ (S[k].y − S[i].y)2, dminsq)
+                # k←k+1
+    # return sqrt(dminsq)
